2ad355318813bb29fd6a8ddd7ead2e55a4eb0227
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-08-05 14:57:09 -0300

    chore: Add initial project configuration files and folders

---
+++ database/migrations/1722879255655_create_users_table.ts
@@ -0,0 +1,19 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'users'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.string('email').unique().notNullable()
+      table.string('password').notNullable()
+      table.timestamp('created_at').defaultTo(this.now())
+      table.timestamp('updated_at').defaultTo(this.now())
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
