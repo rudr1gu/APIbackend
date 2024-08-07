922040c62e71674ef42df49c132f2bb7741a18ef
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-08-07 11:41:09 -0300

    feat: Add show, update, and destroy methods to PostagemsController

--- app/controllers/postagems_controller.ts
+++ app/controllers/postagems_controller.ts
@@ -37,4 +37,56 @@ export default class PostagemsController {
           data: postagem,
         }
       }
+
+    async show({ params, response }: HttpContext) {
+        const postagem = await Postagem.find(params.id)
+    
+        if(!postagem) {
+          response.status(404).json({ message: 'postagem não encontrada' })
+          return
+        }
+    
+        response.status(200).json(postagem)
+    
+        return {
+          message: 'detalhes da postagem',
+          data: postagem,
+        }
+      }
+
+    async update({ params, request, response }: HttpContext) {
+        const body = request.body()
+        const postagem = await Postagem.find(params.id)
+    
+        if(!postagem) {
+          response.status(404).json({ message: 'postagem não encontrada' })
+          return
+        }
+    
+        postagem.merge(body)
+    
+        await postagem.save()
+    
+        response.status(200).json(postagem)
+    
+        return {
+          message: 'postagem atualizada com sucesso!',
+          data: postagem,
+        }
+      }
+
+    async destroy({ params}: HttpContext) {
+        const postagem = await Postagem.find(params.id)
+
+        if(!postagem) {
+          return {
+            message: 'postagem não encontrada',
+          }
+        }
+        await postagem.delete()
+    
+        return {
+          message: 'postagem deletada com sucesso!',
+        }
+      }
 }
