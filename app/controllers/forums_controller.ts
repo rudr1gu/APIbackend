a270bc7b973f7ef7058253dc293aa58d3f945c52
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-09-12 10:30:38 -0300

    Refactor ForumsController to preload 'aluno' and 'materia'

--- app/controllers/forums_controller.ts
+++ app/controllers/forums_controller.ts
@@ -3,7 +3,7 @@ import Forum from '#models/forum'
 
 export default class ForumsController {
     public async index(){
-        const forums = await Forum.query().preload('alunos').preload('materias')
+        const forums = await Forum.query().preload('aluno').preload('materia')
 
         return forums
     }
