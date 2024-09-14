867a794d3e4639826d9a3d1ed805f346a815212b
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-09-14 18:07:31 -0300

    :recycle: Refactor ForumsController to preload 'materia' with 'tags'

--- app/controllers/forums_controller.ts
+++ app/controllers/forums_controller.ts
@@ -3,7 +3,11 @@ import Forum from '#models/forum'
 
 export default class ForumsController {
     public async index(){
-        const forums = await Forum.query().preload('aluno').preload('materia')
+        const forums = await Forum.query()
+            .preload('aluno')
+            .preload('materia', (query) => {
+                query.preload('tags')
+            })
 
         return forums
     }
