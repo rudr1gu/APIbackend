205bc603605d564de317bf401b0e7b8b1474cf3a
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-09-25 20:33:22 -0300

    add ssl in config db

--- config/database.ts
+++ config/database.ts
@@ -12,6 +12,9 @@ const dbConfig = defineConfig({
         user: env.get('DB_USER'),
         password: env.get('DB_PASSWORD'),
         database: env.get('DB_DATABASE'),
+        ssl: {
+          rejectUnauthorized: true,
+        },
       },
       migrations: {
         naturalSort: true,
